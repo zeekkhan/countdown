80f39582ad232971ee8ac2bcbc37a65e1a7c54a0
zeekkhan <zeekkhanz10000@gmail.com>
2024-08-25 14:07:58 +0500

    firebase-set

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIoIVTLVaitvL_axaCBjTwB9kFgXt3mCA",
-  authDomain: "countdownapp-c719f.firebaseapp.com",
-  projectId: "countdownapp-c719f",
-  storageBucket: "countdownapp-c719f.appspot.com",
-  messagingSenderId: "344440139351",
-  appId: "1:344440139351:web:e2541570c73c113f26bd12"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDING_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebasea
